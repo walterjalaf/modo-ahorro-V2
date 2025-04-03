4633177680b74ca20f0df6ed4aff25ca7056a848
carlostaglio <carlostaglio34@gmail.com>
2025-04-02 21:26:35 -0300

    Cambio de logos, base del formulario agregada

--- js/script.js
+++ js/script.js
@@ -39,3 +39,22 @@ document.addEventListener('DOMContentLoaded', function () {
         inputElement.value = question;
     });
 });
+
+// Formulario Whatsapp
+document.getElementById('boxwsp-rselect').addEventListener('change', function () {
+    var diagnosticoOptions = document.getElementById('boxwsp-diagnosticoOptions');
+    diagnosticoOptions.style.display = this.value === 'Diagnóstico Energético' ? 'block' : 'none';
+});
+
+document.getElementById('boxwsp-whatsappButton').addEventListener('click', function () {
+    var name = document.getElementById('boxwsp-rname').value;
+    var interest = document.getElementById('boxwsp-rselect').value;
+    var inmueble = document.getElementById('boxwsp-inmuebleSelect')?.value || '';
+
+    var message = interest === 'Diagnóstico Energético'
+        ? `Hola, soy ${name} y deseo realizar una consulta acerca del servicio de ${interest} para ${inmueble}.`
+        : `Hola, soy ${name} y estoy interesado en ${interest}.`;
+
+    var whatsappUrl = `https://wa.me/+54264154533704?text=${encodeURIComponent(message)}`;
+    window.open(whatsappUrl, '_blank');
+});
